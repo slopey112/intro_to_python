0b6d73da5f9bc5ab5f756d7bf39c488142175834
slopey112 <howard_peng@icloud.com>
2019-10-16 18:28:08 -0700

    rewrite problem 12

--- lesson_6/ps1/problem_12.py
+++ lesson_6/ps1/problem_12.py
@@ -1,5 +1,9 @@
+# You only need to change line 2.
 a = 73
 b = 41
-
-else:
-    print(a)
+c = [1, 2]
+c[0] = a
+c[1] = b
+if c[-1] != 22:
+    raise Exception('Try again.')
+print('Yay! You solved it!')
