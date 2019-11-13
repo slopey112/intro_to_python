d7d09d3fce3865bd0bde53e5ae230db2fda649f0
slopey112 <Howard_Peng@iCloud.com>
2019-11-13 15:40:30 -0800

    add problem 3

---
+++ lesson_8/ps2/problem3.py
@@ -0,0 +1,8 @@
+# Write a function which returns true if the parameter is even, and false if the parameter is odd.
+
+# Testing area.
+assert test(5) == False, "Test case 1 failed."
+assert test(7) == False, "Test case 2 failed."
+assert test(10) == True, "Test case 3 failed."
+assert test(8167) == False, "Test case 4 failed."
+assert test(5316874) == True, "Test case 5 failed."
