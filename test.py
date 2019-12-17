1888852a74b2349b7aace8dfd5f759e6f48574c2
slopey112 <howard_peng@gmail.com>
2019-12-16 17:49:13 -0800

    1 liner

--- test.py
+++ test.py
@@ -1,6 +1,3 @@
 import sys
 
-shift = int(sys.argv[2])
-s = sys.argv[1].upper()
-c = [chr(((ord(i) - 65 + shift) % 26) + 65) if i.isalpha() else i for i in s]
-print(''.join(c))
+print(''.join([chr(((ord(i) - 65 + int(sys.argv[2])) % 26) + 65) if i.isalpha() else i for i in sys.argv[1].upper()]))
